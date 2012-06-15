a00d0003f1d59a94ac666b84f93f6fcbcb1f8fc0
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-06-15 23:38:42 +0100

    erb process yml file

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 require 'active_record'
 require 'yaml'
 require 'logger'
+require 'erb'
 
 #Load database.yml
 config_file = File.expand_path("../database.yml", __FILE__)
@@ -8,7 +9,8 @@ config_file = File.expand_path("../database.yml", __FILE__)
 def env
   @env ||= {}
 end
-env[:dbconfigs] = YAML.load File.read(config_file)
+db_yaml = ERB.new(File.read(config_file)).result(binding)
+env[:dbconfigs] = YAML.load db_yaml
 env[:mode] = ENV["RACK_ENV"] || 'development'
 env[:db] = env[:dbconfigs][env[:mode]]
 
